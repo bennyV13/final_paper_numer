6bcf962f69efefdf34041fc43d9c1754b524a7a7
bennyV13 <bennyv314@duck.com>
2025-02-03 15:36:45 +0200

    A is ok

--- q5.m
+++ q5.m
@@ -1,7 +1,7 @@
 clc;
 clear all;
 close all;
-c=10;
+c=5;
 x=linspace(0,2*pi,c);
 y=linspace(0,1,c);
 dx=x(2)-x(1);
@@ -28,27 +28,39 @@ end
 
 %initialazing the matrix
 A=zeros(c*c);
-b=zeros(c*c,1);
+b=ones(c*c,1);
 q=zeros(c*c,1);
-
+alpha=2*(1/dx^2+1/dy^2)+1/dt;
+beta=-1/dx^2;
+gamma=-1/dy^2;
 
 % for row i theres:
 % alpha on column i
 % beta on column i-1,i+1
 % gamma on column i+n,i-n
-vec_alpha=1/2*ones(c*c-c,1);
-vec_beta=2*ones(c*c-c,1);
-vec_gamma=3*ones(c*c-c,1);
+vec_alpha=alpha*ones(c*c-c,1);
+vec_beta=beta*ones(c*c-c,1);
+vec_gamma=gamma*ones(c*c-c,1);
 A1=spdiags([vec_gamma vec_beta vec_alpha vec_beta vec_gamma], [1 c c+1 c+2 2*c+1 ],c*c-2*(c+1) , c*c);
 A2=spdiags([ones(c+1,1)], [0], c+1,c*c);
 A3=spdiags([ones(c+1,1)], [c*c-c-1], c+1,c*c);
 
 A=[A2;A1;A3];
 
-for ii=c:c*c-c
-    
+
+for ii=1:c
+    if ii>=2
+        A(ii*c:ii*c+1,:)=0;
+        A(ii*c,ii*c)=1;   
+        A(ii*c+1,ii*c+1)=1; 
+    end
+
 end
 
+
+%delete later
+A=full(A);
+disp(A);
 % for ii =1:c
 %     % defining the sections of A that are based on the boundry conditions
 %     A(ii,ii)=1;
@@ -60,4 +72,3 @@ end
 %     b((ii-1)*c+1)=cos((ii-1)*h);
 %     b(ii*c)=cos((ii-1)*h);
 % end
-
