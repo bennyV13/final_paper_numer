7b18b3c450ac87e531348ff4d122dd4731ec6565
bennyV13 <bennyv314@duck.com>
2025-02-03 14:41:48 +0200

    building mat A

---
+++ q5.m
@@ -0,0 +1,63 @@
+clc;
+clear all;
+close all;
+c=10;
+x=linspace(0,2*pi,c);
+y=linspace(0,1,c);
+dx=x(2)-x(1);
+dy=y(2)-y(1);
+dt = 0.0001;
+%grid coordinates
+
+[X,Y]=meshgrid(x,y);
+
+%cylinder coordinates
+phi = linspace(0,2*pi,100);
+R = 0.2;
+cylinder = [pi+R*cos(phi);1/2+R*sin(phi)];
+eps = cylinder(1,:);
+eta = cylinder(2,:);
+%boundary condition on the cylinder surface
+T_cyl = 2;
+
+%initial condition on grid
+TT = [ones(c*c,1)];
+for ii=1:c
+    TT = [ones(c,1)*cos(x(ii));TT];
+end
+
+%initialazing the matrix
+A=zeros(c*c);
+b=zeros(c*c,1);
+q=zeros(c*c,1);
+
+
+% for row i theres:
+% alpha on column i
+% beta on column i-1,i+1
+% gamma on column i+n,i-n
+vec_alpha=1/2*ones(c*c-c,1);
+vec_beta=2*ones(c*c-c,1);
+vec_gamma=3*ones(c*c-c,1);
+A1=spdiags([vec_gamma vec_beta vec_alpha vec_beta vec_gamma], [1 c c+1 c+2 2*c+1 ],c*c-2*(c+1) , c*c);
+A2=spdiags([ones(c+1,1)], [0], c+1,c*c);
+A3=spdiags([ones(c+1,1)], [c*c-c-1], c+1,c*c);
+
+A=[A2;A1;A3];
+
+for ii=c:c*c-c
+    
+end
+
+% for ii =1:c
+%     % defining the sections of A that are based on the boundry conditions
+%     A(ii,ii)=1;
+%     A(c*c-c+ii,c*c-c+ii)=1;
+%     A((ii-1)*c+1,(ii-1)*c+1)=1;
+%     A(ii*c,ii*c)=1;
+%     % defining the parts of b that are based on the boundry conditions
+%     % where y=0 and y=pi/2
+%     b((ii-1)*c+1)=cos((ii-1)*h);
+%     b(ii*c)=cos((ii-1)*h);
+% end
+
