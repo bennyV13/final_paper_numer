b4ff15738fc229aaecfffffaf71aab09629ff2c2
bennyV13 <bennyv314@duck.com>
2025-02-03 16:05:35 +0200

    b is ok

--- q5.m
+++ q5.m
@@ -1,14 +1,15 @@
-clc;
-clear all;
-close all;
-c=5;
+clc; clear all; close all;
+
+%% parameters
+
+c=50; %grid size
 x=linspace(0,2*pi,c);
 y=linspace(0,1,c);
 dx=x(2)-x(1);
 dy=y(2)-y(1);
 dt = 0.0001;
-%grid coordinates
 
+%grid coordinates
 [X,Y]=meshgrid(x,y);
 
 %cylinder coordinates
@@ -20,15 +21,17 @@ eta = cylinder(2,:);
 %boundary condition on the cylinder surface
 T_cyl = 2;
 
-%initial condition on grid
-TT = [ones(c*c,1)];
-for ii=1:c
-    TT = [ones(c,1)*cos(x(ii));TT];
-end
+
+% %initial condition on grid
+% TT = [ones(c*c,1)];
+% for ii=1:c
+%     TT = [ones(c,1)*cos(x(ii));TT];
+% end
+
+%% initialazing mat A and vec b, including IC
 
 %initialazing the matrix
-A=zeros(c*c);
-b=ones(c*c,1);
+b=zeros(c*c,1);
 q=zeros(c*c,1);
 alpha=2*(1/dx^2+1/dy^2)+1/dt;
 beta=-1/dx^2;
@@ -44,31 +47,24 @@ vec_gamma=gamma*ones(c*c-c,1);
 A1=spdiags([vec_gamma vec_beta vec_alpha vec_beta vec_gamma], [1 c c+1 c+2 2*c+1 ],c*c-2*(c+1) , c*c);
 A2=spdiags([ones(c+1,1)], [0], c+1,c*c);
 A3=spdiags([ones(c+1,1)], [c*c-c-1], c+1,c*c);
-
 A=[A2;A1;A3];
 
-
-for ii=1:c
+for ii=1:c 
     if ii>=2
         A(ii*c:ii*c+1,:)=0;
         A(ii*c,ii*c)=1;   
         A(ii*c+1,ii*c+1)=1; 
     end
-
+    % defining the parts of b that are based on the boundry conditions
+    % where x=0 and x=2*pi
+    b((ii-1)*c+1)=1;
+    b(ii*c)=1;
+    disp(b);    
 end
+% defining the parts of b that are based on the boundry conditions where
+% y=0 and y=1
+b(1:c)=cos(2*x(ii))/dt;
+b(c*c-c+1:c*c)=cos(2*x(ii))/dt;
 
 
-%delete later
-A=full(A);
-disp(A);
-% for ii =1:c
-%     % defining the sections of A that are based on the boundry conditions
-%     A(ii,ii)=1;
-%     A(c*c-c+ii,c*c-c+ii)=1;
-%     A((ii-1)*c+1,(ii-1)*c+1)=1;
-%     A(ii*c,ii*c)=1;
-%     % defining the parts of b that are based on the boundry conditions
-%     % where y=0 and y=pi/2
-%     b((ii-1)*c+1)=cos((ii-1)*h);
-%     b(ii*c)=cos((ii-1)*h);
-% end
+
